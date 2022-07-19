a8df3444cb4292c158281ad80856536e9e296110
Thomas Cornell <tjcornell@wustl.edu>
2022-07-19 06:23:14 -0500

    refactored register and RegisterForm

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,6 +1,7 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Center, Heading, Text, Input, InputGroup, Button, InputRightElement, VStack, Alert, AlertIcon, Divider, Link as ChakraLink } from '@chakra-ui/react';
+import * as EmailValidator from 'email-validator';
 
 export default function RegisterForm(props: RegisterFormProps) {
     const [emailBlurred, setEmailBlurred] = useState(false);
@@ -10,26 +11,62 @@ export default function RegisterForm(props: RegisterFormProps) {
     const [showPasswords, setShowPasswords] = useState(false);
     const toggleShowPasswords = () => setShowPasswords(!showPasswords);
 
+    //State for register form
+    const [email, setEmail] = useState('');
+    const handleChangeEmail = (event: FormEvent<HTMLInputElement>) => {
+        const currentValue = event.currentTarget.value;
+        setEmail(currentValue);
+        setEmailValid(EmailValidator.validate(currentValue));
+    }
+
+    const [password, setPassword] = useState('');
+    const handleChangePassword = (event: FormEvent<HTMLInputElement>) => {
+        const currentValue = event.currentTarget.value;
+        setPassword(currentValue);
+        setPasswordLongEnough(currentValue.length >= 6);
+    }
+
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const handleChangeConfirmPassword = (event: FormEvent<HTMLInputElement>) => {
+        setConfirmPassword(event.currentTarget.value);
+    };
+
+    //If either password or confirmPassword change, check if they match and update passwordsMatch variable
+    useEffect(() => {
+        setPasswordsMatch(password === confirmPassword);
+    }, [password, confirmPassword]);
+
+    //Checks before registration sent
+    const [emailValid, setEmailValid] = useState(false);
+    const [passwordLongEnough, setPasswordLongEnough] = useState(false);
+    const [passwordsMatch, setPasswordsMatch] = useState(true);
+    const [registrationValid, setRegistrationValid] = useState(emailValid && passwordLongEnough && passwordsMatch);
+
+    //emailValid, passwordsMatch, and passwordLongEnough are the 3 requirements for valid Registration. If any of these variables change, update registrationValid accordingly
+    useEffect(() => {
+        setRegistrationValid(emailValid && passwordsMatch && passwordLongEnough);
+    }, [emailValid, passwordsMatch, passwordLongEnough]);
+
     const submitForm = (event: FormEvent) => {
         event.preventDefault();
-        if (props.registrationValid) {
-            props.handleSignUpForm();
+        if (registrationValid) {
+            props.handleSignUpForm(email, password);
         }
     };
 
     return (
         <VStack>
-            {(!props.emailValid && emailBlurred && props.email.length > 0) &&
+            {(!emailValid && emailBlurred && email.length > 0) &&
                 <Alert status='error'>
                     <AlertIcon />
                     Email invalid! Please use a valid email address.
                 </Alert>}
-            {(!props.passwordsMatch && passwordBlurred && confirmPasswordBlurred && props.password.length > 0 && props.confirmPassword.length > 0) &&
+            {(!passwordsMatch && passwordBlurred && confirmPasswordBlurred && password.length > 0 && confirmPassword.length > 0) &&
                 <Alert status='error'>
                     <AlertIcon />
                     Your passwords must match!
                 </Alert>}
-            {(!props.passwordLongEnough && passwordBlurred && confirmPasswordBlurred && props.password.length > 0) &&
+            {(!passwordLongEnough && passwordBlurred && confirmPasswordBlurred && password.length > 0) &&
                 <Alert status='error'>
                     <AlertIcon />
                     Your password must be at least 6 characters long!
@@ -44,8 +81,8 @@ export default function RegisterForm(props: RegisterFormProps) {
                     <Divider borderColor='gray.400' />
                     <form onSubmit={submitForm}>
                         <Input
-                            value={props.email}
-                            onChange={props.handleChangeEmail}
+                            value={email}
+                            onChange={handleChangeEmail}
                             onBlur={() => setEmailBlurred(true)}
                             onFocus={() => setEmailBlurred(false)}
                             placeholder='Enter email'
@@ -58,8 +95,8 @@ export default function RegisterForm(props: RegisterFormProps) {
                             <Input
                                 pr='4.5rem'
                                 type={showPasswords ? 'text' : 'password'}
-                                value={props.password}
-                                onChange={props.handleChangePassword}
+                                value={password}
+                                onChange={handleChangePassword}
                                 onBlur={() => setPasswordBlurred(true)}
                                 onFocus={() => setPasswordBlurred(false)}
                                 placeholder='Enter password'
@@ -83,8 +120,8 @@ export default function RegisterForm(props: RegisterFormProps) {
                             <Input
                                 pr='4.5rem'
                                 type={showPasswords ? 'text' : 'password'}
-                                value={props.confirmPassword}
-                                onChange={props.handleChangeConfirmPassword}
+                                value={confirmPassword}
+                                onChange={handleChangeConfirmPassword}
                                 onBlur={() => setConfirmPasswordBlurred(true)}
                                 onFocus={() => setConfirmPasswordBlurred(false)}
                                 placeholder='Confirm password'
@@ -103,7 +140,7 @@ export default function RegisterForm(props: RegisterFormProps) {
                             </InputRightElement>
                         </InputGroup>
                     </form>
-                    <Button onClick={submitForm} isDisabled={!props.registrationValid} colorScheme='blue'>Register</Button>
+                    <Button onClick={submitForm} isDisabled={!registrationValid} colorScheme='blue'>Register</Button>
                     <Text align='center'>
                         Already have an account? <Link href='/login' passHref><ChakraLink color='green'>Sign In</ChakraLink></Link> instead.
                     </Text>
